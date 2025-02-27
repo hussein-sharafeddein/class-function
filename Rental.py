96414ad066b84bba81b9414c28430c952f08bfb4
Rabbit-Striker <husseinsharafeddein@gmail.com>
2025-02-27 21:05:12 +0200

    Create ==> Class and init method

---
+++ Rental.py
@@ -0,0 +1,6 @@
+Class Vehicle:
+    def __init__(self, brand, model, year, rental_price_per_day):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.__rental_price_per_day = rental_price_per_day
