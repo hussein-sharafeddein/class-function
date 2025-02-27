9359d68dd10e97e62c1fd29ff91cd98f93844cfe
Rabbit-Striker <husseinsharafeddein@gmail.com>
2025-02-27 21:10:12 +0200

    Create ==> display_info method

--- Rental.py
+++ Rental.py
@@ -4,3 +4,9 @@ Class Vehicle:
         self.model = model
         self.year = year
         self.__rental_price_per_day = rental_price_per_day
+
+    def display_info(self):
+        print(f"Brand: {self.brand}")
+        print(f"Model: {self.model}")
+        print(f"Year: {self.year}")
+        print(f"Rental Price Per Day: {self.__rental_price_per_day}")
