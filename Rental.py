9abe5408b394423d1dc9a50a307505bcb6af895e
Rabbit-Striker <husseinsharafeddein@gmail.com>
2025-03-02 15:37:33 +0200

    Create ==> Subclasses Car and Bike

--- Rental.py
+++ Rental.py
@@ -5,6 +5,7 @@ Class Vehicle:
         self.year = year
         self.__rental_price_per_day = rental_price_per_day
 
+        #Added display_info method to display the information of the vehicle
     def display_info(self):
         print(f"Brand: {self.brand}")
         print(f"Model: {self.model}")
@@ -14,8 +15,20 @@ Class Vehicle:
     def calculate_rental_cost(self, days):
         return self.__rental_price_per_day * days
 
+        #Added setter method for rental_price_per_day
     def set_rental_price_per_day(self, price):
         self.__rental_price_per_day = price
 
+        #Added getter method for rental_price_per_day
     def get_rental_price_per_day(self):
         return self.__rental_price_per_day
+
+Class Car(Vehicle):
+    def __init__(self, brand, model, year, rental_price_per_day, seat_capacity): #Added additional attribute: seating_capacity for Car class
+        self.seat_capacity = seat_capacity
+        super().__init__(brand, model, year, rental_price_per_day)
+
+Class Bike(Vehicle):
+    def __init__(self, brand, model, year, rental_price_per_day, engine_capacity): #Added additional attribute: engine_capacity for Bike class
+        self.engine_capacity = engine_capacity
+        super().__init__(brand, model, year, rental_price_per_day)
